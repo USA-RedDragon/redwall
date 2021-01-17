ce560bfd8df6436d965dd692d7177aaaba0cd140
Jacob McSwain <jacob@mcswain.dev>
2021-01-17 02:26:34 -0600

    Use file templating to set nat loopback on firewall

--- internal/firewall/firewall.go
+++ internal/firewall/firewall.go
@@ -1,7 +1,11 @@
 package firewall
 
 import (
+	"fmt"
 	"net"
+	"os"
+
+	"os/exec"
 
 	"github.com/USA-RedDragon/redwall/internal/iplistener"
 	"k8s.io/klog/v2"
@@ -20,20 +24,38 @@ func (f *Firewall) updateNATReflection(newIP net.IP) {
 			f.current4IP = newIP
 			klog.Infof("IP Changed. Updating NAT4 Reflection Rule")
 			// Update reflection
-			// iifname lan ip daddr $public_ip tcp dport {80, 443} dnat $nginx_server
+			f, err := os.OpenFile("/etc/nftables/public_ip.nft", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0755)
+			if err != nil {
+				klog.Fatal(err)
+			}
+			_, err = f.WriteString(fmt.Sprintf("define public_ip = %s\n", newIP.String()))
+			if err != nil {
+				klog.Fatal(err)
+			}
+			if err := f.Close(); err != nil {
+				klog.Fatal(err)
+			}
+			cmd := exec.Command("nft", "-f", "/etc/nftables.conf")
+			err = cmd.Run()
+			if err != nil {
+				klog.Errorf("Failed to setup firewall: %v", err)
+			}
 		}
 	}
 }
 
 func (f *Firewall) Start() {
 	klog.Info("Configuring firewall")
-
-	// Run nft on host with templated file
-
-	klog.Info("Firewall setup")
-
 	f.updateNATReflection(f.current4IP)
 	f.ip4listener.Subscribe(f.updateNATReflection)
+
+	cmd := exec.Command("nft", "-f", "/etc/nftables.conf")
+	err := cmd.Run()
+	if err != nil {
+		klog.Errorf("Failed to setup firewall: %v", err)
+	}
+
+	klog.Info("Firewall setup")
 }
 
 func New(current4IP net.IP, ip4listener *iplistener.IPListener) *Firewall {
